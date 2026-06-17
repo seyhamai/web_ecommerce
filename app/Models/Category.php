9e67e558eb2f150c64e3e4ea335991305d3df59d
seyhamai <156148727+seyhamai@users.noreply.github.com>
2026-06-17 08:38:30 +0700

    Implement getFullPathAttribute method in Category model
    Added a method to get the full path of the category.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    // 1. Allow mass assignment for your form
+    
     protected $fillable = [
         'parent_id',
         'name',
@@ -19,21 +19,25 @@ class Category extends Model
         'is_active'
     ];
 
-    /**
-     * Get the parent category (Points UP)
-     * Usage: $category->parent->name
-     */
-    public function parent()
+     public function parent()
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    /**
-     * Get the subcategories (Points DOWN)
-     * Usage: $category->children
-     */
     public function children()
     {
         return $this->hasMany(Category::class, 'parent_id');
+    }
+    public function getFullPathAttribute()
+    {
+        $names = [];
+        $category = $this;
+        
+        while ($category) {
+            $names[] = $category->name;
+            $category = $category->parent;
+        }
+        
+        return implode(' > ', array_reverse($names));
     }
 }
